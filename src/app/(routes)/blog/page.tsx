49cbb65d9ad81c13df12a4fdd03e0a4d07d0e4d4
George <xsova113@gmail.com>
2023-10-14 15:12:31 -0700

    bug fix attempt 2

--- src/app/(routes)/blog/page.tsx
+++ src/app/(routes)/blog/page.tsx
@@ -30,7 +30,7 @@ export default function Blog() {
     <div className="flex flex-col items-center mt-24">
       <h1 className="md:text-5xl text-3xl font-semibold mb-16">BLOG</h1>
       <div className="flex mr-auto gap-x-12 w-full">
-        {/* <SortAndFilter /> */}
+        <SortAndFilter />
         <div className="flex flex-col w-full gap-4">
           <div className="flex justify-between">
             {/* <MobileSortAndFilter /> */}
