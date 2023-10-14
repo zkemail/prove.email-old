b30d7e3875483454bdedccf2d0a6b592640a1902
George <xsova113@gmail.com>
2023-10-14 15:17:28 -0700

    bug fix attempt

--- src/app/(routes)/blog/page.tsx
+++ src/app/(routes)/blog/page.tsx
@@ -30,10 +30,10 @@ export default function Blog() {
     <div className="flex flex-col items-center mt-24">
       <h1 className="md:text-5xl text-3xl font-semibold mb-16">BLOG</h1>
       <div className="flex mr-auto gap-x-12 w-full">
-        <SortAndFilter />
+        {/* <SortAndFilter /> */}
         <div className="flex flex-col w-full gap-4">
           <div className="flex justify-between">
-            {/* <MobileSortAndFilter /> */}
+            <MobileSortAndFilter />
             <Input
               placeholder="Search on blog..."
               onChange={(e) => setSearchInput(e.target.value)}
