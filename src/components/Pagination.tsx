9c086885144384deddd782e85ca01685655068fd
George <xsova113@gmail.com>
2023-10-16 16:03:14 -0700

    minor blog page update

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -26,17 +26,22 @@ const Pagination = ({ posts }: PaginationProps) => {
   return (
     <div className="flex flex-col w-full mb-20">
       <div className="flex flex-col gap-y-10 w-full mb-10">
+        {!subset.length && (
+          <p className="self-center py-20 text-lg">No posts found...</p>
+        )}
         {subset.map((item) => (
           <BlogCard key={item._id} post={item} />
         ))}
       </div>
-      <ReactPaginate
-        pageCount={pageCount}
-        onPageChange={handlePageChange}
-        containerClassName={"flex items-center justify-center gap-8 mt-auto"}
-        activeClassName="bg-secondary py-1 px-2 border rounded"
-        disabledClassName="opacity-40"
-      />
+      {!subset.length ? null : (
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={handlePageChange}
+          containerClassName={"flex items-center justify-center gap-8 mt-auto"}
+          activeClassName="bg-secondary py-1 px-2 border rounded"
+          disabledClassName="opacity-40"
+        />
+      )}
     </div>
   );
 };
