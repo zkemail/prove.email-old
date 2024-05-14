b14a4a3a6c0db4b7b6dc90fdf6660cf8213af6c9
Divide-By-0 <aayushg@mit.edu>
2024-05-14 19:03:23 +0900

    all proof of email -> zk email

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "ZK Email",
-  description: "Proof of Email allows for trust-minimized identity proofs, on/off-ramp flows, and unique email-based on-chain interactions",
+  description: "ZK Email lets you prove any subset of any email on-chain. It allows for trust-minimized identity proofs, on/off-ramp flows, and unique email-based on-chain interactions",
 };
 
 const font = Inter({ subsets: ["latin"] });
--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Proof of Email',
+  title: 'ZK Email',
   description: 'Prove any existing emails in your inbox on-chain with Ethereum, secured by advanced cryptography. Create anonymous proofs of identity, send money to email addresses, and more with zk email.',
 }
